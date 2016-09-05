3d045da1f7dc928d6b8c52efa3570f82e5643bc9
Jennifer <jennifersoden@hotmail.com>
2016-09-04 18:35:28 -0600

    Add invoice items, invoices and items controllers Add invoice items, invoices and items models Add invoice items, invoices and items migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: {format: :json} do
+      resources :invoices, only: [:show, :index]
+      resources :invoice_items, only: [:show, :index]
+      resources :items, only: [:show, :index]
       resources :merchants, only: [:show, :index] do
         collection do
           get :find
           get :find_all
         end
       end
-
+    end
   end
-end
-
 end
