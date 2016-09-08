7b284cacea64964a78d87f9e54a4472661685075
Jennifer <jennifersoden@hotmail.com>
2016-09-07 20:35:33 -0600

    Add jbuilder to sanitize transactions

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::TransactionsController < Api::ApiController
   respond_to :json
   def index
-    respond_with Transaction.all
+    @transactions = Transaction.all
+    respond_with @transactions
   end
 
   def show
     respond_with Transaction.find(params[:id])
-  end
-
-  def invoice
-    @transaction = Transaction.find(params[:transaction_id])
-    respond_with @transaction.invoice
   end
 end
