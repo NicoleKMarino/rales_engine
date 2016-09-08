38009fc2bb6c8c1b0780c89948783672d584c0a9
Nicole Marino <nicolekmarino@hotmail.com>
2016-09-07 19:57:26 -0600

    passing specs for invoice_item relationship

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
       get "invoice_items/find_all", to: "invoice_items/find#index"
       get "invoice_items/find", to: "invoice_items/find#show"
       get "invoice_items/random", to: "invoice_items/random#show"
+      get 'invoice_items/:id/invoice', to: "invoice_items/invoices#show"
+      get 'invoice_items/:id/item', to: "invoice_items/items#show"
 
-      resources :invoice_items, only: [:show, :index] do
-        get :invoice
-        get :item
-      end
+      resources :invoice_items, only: [:show, :index]
 
       get "invoices/find_all", to: "invoices/find#index"
       get "invoices/find", to: "invoices/find#show"
